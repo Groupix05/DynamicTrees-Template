aef79afd65ad1a54dda2a69198137ad959516898
Groupix05 <fox.bouche2@gmail.com>
2025-01-23 01:57:28 +0100

    Port to neoforge 1.21.1
    This version is not clean at all, but it works

--- src/main/java/dttemplate/DynamicTreesTemplate.java
+++ src/main/java/dttemplate/DynamicTreesTemplate.java
@@ -1,24 +1,31 @@
 package dttemplate;
 
-import com.ferreusveritas.dynamictrees.api.GatherDataHelper;
-import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.data.event.GatherDataEvent;
+import com.dtteam.dynamictrees.registry.NeoForgeRegistryLoader;
+import com.dtteam.dynamictrees.treepack.Resources;
+import com.dtteam.dynamictrees.event.handler.OptionalHandlers;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @Mod(DynamicTreesTemplate.MOD_ID)
 public final class DynamicTreesTemplate {
 
     public static final String MOD_ID = "dttemplate";
+    public static IEventBus MOD_EVENT_BUS;
 
-    public DynamicTreesTemplate() {
-        RegistryHandler.setup(MOD_ID);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::gatherData);
+    public DynamicTreesTemplate(IEventBus eventBus, ModContainer container) {
+        MOD_EVENT_BUS = eventBus;
+
+        eventBus.addListener(this::gatherData);
+        NeoForgeRegistryLoader.setup(eventBus);
+        MOD_EVENT_BUS = null;
     }
 
     private void gatherData(final GatherDataEvent event) {
-        GatherDataHelper.gatherTagData(MOD_ID, event);
-        GatherDataHelper.gatherLootData(MOD_ID, event);
+        Resources.MANAGER.gatherData();
     }
 
 }
