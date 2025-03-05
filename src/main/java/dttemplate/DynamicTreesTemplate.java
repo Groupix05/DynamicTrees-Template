99f28ca995e7a446cf3e979b7726020635126f64
Groupix05 <fox.bouche2@gmail.com>
2025-03-05 16:18:23 +0100

    Fix Data generation

--- src/main/java/dttemplate/DynamicTreesTemplate.java
+++ src/main/java/dttemplate/DynamicTreesTemplate.java
@@ -2,9 +2,22 @@ package dttemplate;
 
 import com.ferreusveritas.dynamictrees.api.GatherDataHelper;
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.ferreusveritas.dynamictrees.block.leaves.LeavesProperties;
+import com.ferreusveritas.dynamictrees.block.rooty.SoilProperties;
+import com.ferreusveritas.dynamictrees.resources.Resources;
+import com.ferreusveritas.dynamictrees.systems.fruit.Fruit;
+import com.ferreusveritas.dynamictrees.systems.pod.Pod;
+import com.ferreusveritas.dynamictrees.tree.family.Family;
+import com.ferreusveritas.dynamictrees.tree.species.Species;
+//import com.ferreusveritas.dynamictreesplus.block.mushroom.CapProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(DynamicTreesTemplate.MOD_ID)
 public final class DynamicTreesTemplate {
@@ -12,13 +25,43 @@ public final class DynamicTreesTemplate {
     public static final String MOD_ID = "dttemplate";
 
     public DynamicTreesTemplate() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::commonSetup);
+        eventBus.addListener(this::clientSetup);
+        eventBus.addListener(this::gatherData);
+
+        //MinecraftForge.EVENT_BUS.register(this); //if your addons requires to interact with the game while running
+
         RegistryHandler.setup(MOD_ID);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::gatherData);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        //DTTemplateRegistries.setup();
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        //DTTemplateClient.setup();
     }
 
     private void gatherData(final GatherDataEvent event) {
-        GatherDataHelper.gatherTagData(MOD_ID, event);
-        GatherDataHelper.gatherLootData(MOD_ID, event);
+        /**
+        * Generate the required data with runData, make sure the mod your are making an addon is in the build.gradle: add "--existing-mod", "your-mod" in create(data)
+        */
+
+        //Resources.MANAGER.gatherData();
+        GatherDataHelper.gatherAllData(MOD_ID, event,
+                SoilProperties.REGISTRY,
+                Family.REGISTRY,
+                Species.REGISTRY,
+                LeavesProperties.REGISTRY,
+                Fruit.REGISTRY,
+                Pod.REGISTRY
+                //,CapProperties.REGISTRY //Requires DTPlus
+        );
+    }
+
+    public static ResourceLocation location(final String path) {
+        return new ResourceLocation(MOD_ID, path);
     }
 
 }
